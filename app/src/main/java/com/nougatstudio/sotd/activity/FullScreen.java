299b0ec96742ae7e67b160923ddb32aca78a4533
sourabh <sourabhkhurrana@gmail.com>
2017-08-18 01:30:05 +0530

    initial commit

---
+++ app/src/main/java/com/nougatstudio/sotd/activity/FullScreen.java
@@ -0,0 +1,60 @@
+package com.nougatstudio.sotd.activity;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.nougatstudio.sotd.R;
+import com.nougatstudio.sotd.adapters.SliderAdaptor;
+
+import java.util.ArrayList;
+
+public class FullScreen extends AppCompatActivity {
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] SlideImages= {R.drawable.b,R.drawable.e,R.drawable.b,R.drawable.f,R.drawable.e,R.drawable.b};
+    private ArrayList<Integer> SlideImageArray = new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_full_screen);
+        init();
+    }
+
+    private void init() {
+        for(int i=0;i<SlideImages.length;i++)
+            SlideImageArray.add(SlideImages[i]);
+
+        mPager = (ViewPager) findViewById(R.id.viewPager);
+        mPager.setAdapter(new SliderAdaptor(FullScreen.this,SlideImageArray));
+//        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+//        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+//        final Handler handler = new Handler();
+//        final Runnable Update = new Runnable() {
+//            public void run() {
+//                if (currentPage == SlideImages.length) {
+//                    currentPage = 0;
+//                }
+//                mPager.setCurrentItem(currentPage++, true);
+//            }
+//        };
+//        Timer swipeTimer = new Timer();
+//        swipeTimer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                handler.post(Update);
+//            }
+//        }, 2500, 2500);
+    }
+}
---
+++ app/src/main/java/com/nougatstudio/sotd/adapters/SliderAdaptor.java
@@ -0,0 +1,54 @@
+package com.nougatstudio.sotd.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nougatstudio.sotd.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sourabh on 8/18/2017.
+ */
+
+public class SliderAdaptor extends PagerAdapter {
+
+    private ArrayList<Integer> images;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public SliderAdaptor(Context context, ArrayList<Integer> images) {
+        this.context = context;
+        this.images=images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View myImageLayout = inflater.inflate(R.layout.slide, view, false);
+        ImageView myImage = (ImageView) myImageLayout
+                .findViewById(R.id.slideImage);
+        myImage.setImageResource(images.get(position));
+        view.addView(myImageLayout, 0);
+        return myImageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+}
