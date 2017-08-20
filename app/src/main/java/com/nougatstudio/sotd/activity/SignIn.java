143269f8de779f938fd40157bb04763c2d63fa7d
NirajSolanki <niraj.solanki.121@gmail.com>
2017-08-20 23:53:37 +0530

    Added Login XML

---
+++ app/src/main/java/com/nougatstudio/sotd/activity/SignIn.java
@@ -0,0 +1,15 @@
+package com.nougatstudio.sotd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nougatstudio.sotd.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
