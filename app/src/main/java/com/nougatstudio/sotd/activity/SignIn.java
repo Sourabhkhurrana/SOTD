9cc5d18bb2d2ee12b1676a4e9a05049640cb3808
pradeep <ps0689@gmail.com>
2017-08-21 00:59:55 +0530

    Added Button In Sign In

--- app/src/main/java/com/nougatstudio/sotd/activity/SignIn.java
+++ app/src/main/java/com/nougatstudio/sotd/activity/SignIn.java
@@ -1,12 +1,17 @@
 package com.nougatstudio.sotd.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.nougatstudio.sotd.R;
+import com.nougatstudio.sotd.nougatstudio.Blur;
 
 public class SignIn extends AppCompatActivity {
-
+ImageView backgroundimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
